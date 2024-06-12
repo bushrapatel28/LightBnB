a8c81cf96397bd90f45cc51e5236db9b68e9ae12
Bushra Patel <bpatel60@yahoo.com>
2024-06-12 16:53:53 -0400

    Fix the thumbnail_photo_url and the cover_photo_url values for the properties table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,9 +4,9 @@ VALUES ('Potato Nekochan', 'pot_cat@billa.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD
 ('Alex Dartmouth', 'admouth22@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 'Farmhouse', 'description', 'https://google_images/cat_cat.png', 'https://google_images/big_farm.png', 100, 3, 3, 4, 'Canada', 'Dream Street', 'Caledon', 'Ontario', 'A1B 2C3', TRUE),
-(2, 'Dark Corner', 'description', 'https://google_images/some_place.png', 'https://google_images/dim_light.png', 150, 1, 3, 3, 'Canada', 'Cozy Street', 'Toronto', 'Ontario', 'A3B 5C3', TRUE),
-(2, 'Time Machine', 'description', 'https://google_images/vintage_house.png', 'https://google_images/wooden_furniture.png', 300, 2, 4, 5, 'Canada', 'Old Young Street', 'Orono', 'Ontario', 'K2J 7D8', TRUE);
+VALUES (1, 'Farmhouse', 'description', 'https://blog.newhomesource.com/wp-content/uploads/2019/09/farmhouse.jpg.webp', 'https://www.agriculture.com/thmb/oE4bSK332mcmqXClPVTQPIVElwM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Smith.Rick_.126-2000-51bf0dff878d43338c4fc21c0d9b5295.jpg', 100, 3, 3, 4, 'Canada', 'Dream Street', 'Caledon', 'Ontario', 'A1B 2C3', TRUE),
+(2, 'Dark Corner', 'description', 'https://i.pinimg.com/originals/64/41/b9/6441b994edbd71f2754c17fb733af948.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnS7g3g6Qtjq-yMsO0ffbuiw7SAI0wWwTng&s', 150, 1, 3, 3, 'Canada', 'Cozy Street', 'Toronto', 'Ontario', 'A3B 5C3', TRUE),
+(2, 'Time Machine', 'description', 'https://media.istockphoto.com/id/182786914/photo/arts-and-crafts-bungelow-house-residential-exterior-facade-midwest-usa.jpg?s=612x612&w=0&k=20&c=9sr2BUCvb-sArGsqwnWgI20xeh4Sq3VxGowLstbmzDk=', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,w_730/at%2Fhouse%20tours%20stock%20archive%2F4a6a179a87e062882fbfb62f51be7d4aeb257030', 300, 2, 4, 5, 'Canada', 'Old Young Street', 'Orono', 'Ontario', 'K2J 7D8', TRUE);
 
 INSERT INTO reservations (guest_id, property_id, start_date, end_date)
 VALUES (3, 1, '2022-06-10', '2022-06-16'),
