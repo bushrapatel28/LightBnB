b69048b5142acd08159b84ae0c13e7e12e4da606
Bushra Patel <bpatel60@yahoo.com>
2024-06-13 01:00:36 -0400

    Make corrections for price range in the query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -128,7 +128,7 @@ const getAllProperties = function (options, limit = 10) {
   }
 
   if (options.minimum_price_per_night) {
-    queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
     if (queryParams.length > 1) {
       queryString += ` AND cost_per_night >= $${queryParams.length}`;
     } else {
@@ -138,7 +138,7 @@ const getAllProperties = function (options, limit = 10) {
   }
 
   if (options.maximum_price_per_night) {
-    queryParams.push(`${options.maximum_price_per_night}`);
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
     if (queryParams.length > 1) {
       queryString += ` AND cost_per_night <= $${queryParams.length}`;
     } else {
